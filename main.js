3f53ea85504ad942474e356219ea39118f5a4447
AMINE.AALI <ma.aali@linkbynet.com>
2020-10-26 17:17:47 +0100

    adding comments

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const path = require('path');
 function parseINI(fileName) {
 
     let str = fs.readFileSync(fileName).toString();
-    let pattern = /^((?!;).)+$/gm; // Match without comments
+    let pattern = /^((?!;).)+$/gm; // Match without comments ";"
     let result = str.match(pattern);
     let newSectionData;
     let newSectionName;
@@ -12,22 +12,21 @@ function parseINI(fileName) {
 
     for (let i = 0; i < result.length; i++) {
         console.log(result[i]);
-        let isSection = /^\[+?/.test(result[i]); // patern is object
+        let isSection = /^\[+?/.test(result[i]); // Regex to check if line is section "[...]"
         if (!isSection && newSectionData == null) {
-            let split = result[i].match(/(\w+)*=*(\w+)/gm); // extract delimiter =
+            let split = result[i].match(/(\w+)*=*(\w+)/gm); // Regex to extract data between "="
             writtenObject[split[0]] = split[1];
         }
         else if (isSection) {
-            if (newSectionData != null && Object.keys(newSectionData).keys > 0) {
+            if (newSectionData != null && Object.keys(newSectionData).keys > 0)
                 writtenObject[newSectionName] = newSectionData;
-            }
             newSectionData = {};
+            // Regex to extrat section name which is between '[' and ']'
             newSectionName = result[i].match(/(?<=\[)(.*?)(?=\])/gm)[0].toString();
-            console.log("SECTION NAME : " + newSectionName)
             writtenObject[newSectionName] = {};
         }
         else if (!isSection && Object.keys(newSectionData).length == 0) {
-            let splitForSection = result[i].match(/(\w+)*=*(\w+)/gm); // extract delimiter =
+            let splitForSection = result[i].match(/(\w+)*=*(\w+)/gm); // Regex to extract data between "="
             writtenObject[newSectionName][splitForSection[0]] = splitForSection[1];
         }
         
